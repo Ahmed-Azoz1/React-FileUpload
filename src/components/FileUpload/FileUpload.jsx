a120f455b6e930d6adc4bc27354b350ea74d2a35
Ahmed-Azoz1 <ahmedhjg123@gmail.com>
2024-07-03 22:30:45 +0300

    Add TailwindCss & Build FileUpload

--- src/components/FileUpload/FileUpload.jsx
+++ src/components/FileUpload/FileUpload.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 
 const FileUpload = () => {
 
+    const inputRef = useRef();
+
+    // State المدخلات
+    const [selectedFile,setSelectedFile] = useState(null);
+    const [progress,setProgress] = useState();
+    const [uploadStatus,setUploadStatus] = useState("select");   // تغيير الحالة الى  "select" , "uploading" , "done"
+    
+    // handle file تغيير الحالة
+    const handleFileChange = (e)=>{
+        if (e.target.files && e.target.files.length > 0){
+            setSelectedFile(e.target.files[0]);
+        }
+    }
+
+
     return (
         <div>
-            FileUpload
+            <input type="file" ref={inputRef} onChange={handleFileChange} className="hidden" />
         </div>
     )
 }
