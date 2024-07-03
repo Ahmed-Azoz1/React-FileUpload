fc3091941425ecae86445d942a441ecbb79f697c
Ahmed-Azoz1 <ahmedhjg123@gmail.com>
2024-07-03 22:29:39 +0300

    New Project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import FileUpload from './components/FileUpload/FileUpload'
+
+
+function App() {
+  return (
+    <div className='container'>
+      <FileUpload />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/FileUpload/FileUpload.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const FileUpload = () => {
+
+    return (
+        <div>
+            FileUpload
+        </div>
+    )
+}
+
+export default FileUpload
